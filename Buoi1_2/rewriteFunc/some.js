5456171c7c82ca8a3c5a5d79f93056c622f3e0d1
H51M14\Student <Student@gmail.com>
2024-08-26 15:46:16 +0700

    done filter

---
+++ Buoi1_2/rewriteFunc/some.js
@@ -0,0 +1,13 @@
+//rewrite function some
+Array.prototype.mySome = function(num) {
+    for (let i = 0; i < num.length; i++) {
+        if (callback(num[i])) {
+            return true;
+        }
+    }
+    return false;
+}
+
+const number = [45, 4, 9, 16, 25];
+var value = number.mySome(18);
+console.log(value);
