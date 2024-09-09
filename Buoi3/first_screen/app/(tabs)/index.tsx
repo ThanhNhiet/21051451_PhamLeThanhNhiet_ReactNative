d9fcecf783809708fa18ad7abecb92747078509d
ThanhNhiet <nhiethiz@gmail.com>
2024-09-09 21:45:04 +0700

    done first_screen

---
+++ Buoi3/first_screen/app/(tabs)/index.tsx
@@ -0,0 +1,96 @@
+import React from 'react';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      {/* Vùng 1: Hình tròn */}
+      <View style={styles.circle} ></View>
+
+      {/* Vùng 2: Tiêu đề */}
+      <View style={styles.titleContainer}>
+        <Text style={styles.title}>GROW</Text>
+        <Text style={styles.title}>YOUR BUSINESS</Text>
+      </View>
+
+      {/* Vùng 3: Mô tả */}
+      <View style={styles.descriptionContainer}>
+        <Text style={styles.description}>
+          We will help you to grow your business using online server
+        </Text>
+      </View>
+
+      {/* Vùng 4: Các nút */}
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>LOGIN</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>SIGN UP</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#00BFFF',
+    alignItems: 'center',
+  },
+
+  circle: {
+    marginTop: 80,
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    borderWidth: 10,
+  },
+
+  titleContainer: {
+    marginTop: 100,
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: 'black',
+  },
+
+  descriptionContainer: {
+    alignItems: 'center',
+  },
+  description: {
+    marginTop: 100,
+    fontSize: 16,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: 'black',
+  },
+
+  buttonContainer: {
+    marginTop: 220,
+    flexDirection: 'row',
+    
+    // justifyContent: 'space-around',
+  },
+  button: {
+    backgroundColor: '#FFD700',
+    paddingVertical: 15,
+    paddingHorizontal: 30,
+    alignItems: 'center',
+    borderRadius: 10,
+    width: 130,
+    height: 50,
+    marginHorizontal: 40,
+  },
+  buttonText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: 'black',
+  },
+});
+
+export default App;
